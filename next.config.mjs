2354a0667f0428590f1f4f353ddc5638aeaab5ea
Kayode Raimi <kayode.raimi123@gmail.com>
2024-07-09 21:54:32 +0100

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**tailwindui.com',
+                port: '',
+                pathname: '/img/logos/**',
+            },
+
+        ],
+    },
+};
+
+export default nextConfig;
