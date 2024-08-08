6d46a16fdbaae33e1b6dad61390ca72d516b505c
Kayode Raimi <kayode.raimi123@gmail.com>
2024-08-08 16:10:00 +0100

    allposts bug 3

--- src/app/api/post/route.tsx
+++ src/app/api/post/route.tsx
@@ -107,8 +107,9 @@ export async function GET(req: NextRequest) {
     // Retrieve all posts from the database
     const posts = await Post.find()
       .sort({ date: "desc" })
+      .limit(10) // Adjust limit as needed
       .populate("author")
-      .populate({ path: 'tags', select: 'name' });
+      .populate("tags");
 
     if (!posts || posts.length === 0) {
       return NextResponse.json({ message: "No posts found" }, { status: 404 });
